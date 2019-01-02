5d86b6a2975e1692be298dfc07fd1239aed0d83d
Seanba <sean@seanba.com>
2019-01-01 18:38:25 -0600

    Deployment files
    Aseprite2Unity can now be deployed with a batch file.

---
+++ Aseprite2Unity/Assets/Aseprite2Unity/Editor/Deploy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+
+namespace Aseprite2Unity.Editor
+{
+    internal static class Deploy
+    {
+        private static void DeployAseprite2Unity()
+        {
+            var path = string.Format("{0}/../../deploy/Aseprite2Unity.{1}.unitypackage", Application.dataPath, AsepriteImporter.Version);
+
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+            AssetDatabase.ExportPackage("Assets/Aseprite2Unity", path, ExportPackageOptions.Recurse);
+        }
+    }
+}
---
+++ deploy/export-aseprite-2-unity.bat
@@ -0,0 +1,14 @@
+@echo off
+pushd %~dp0
+
+set UnityExe="C:\Program Files\Unity\Hub\Editor\2018.3.0f2\Editor\Unity.exe"
+set UnityProj="../Aseprite2Unity"
+set UnityMethod=Aseprite2Unity.Editor.Deploy.DeployAseprite2Unity
+
+echo Deploying Aseprite2Unity
+echo Using Editor: %UnityExe%
+
+start /wait "" %UnityExe% -quit --nographics -batchmode -projectPath %UnityProj% -executeMethod %UnityMethod% -logFile output.log
+echo Done!
+
+popd
