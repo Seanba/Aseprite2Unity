368e1f79e6243aa5e45ea161344730096d5adb3b
Seanba <sean@seanba.com>
2019-09-01 21:11:58 -0500

    Updating project to 2019.2

--- Aseprite2Unity/Assets/Aseprite2Unity/Editor/AsepriteImporter.cs
+++ Aseprite2Unity/Assets/Aseprite2Unity/Editor/AsepriteImporter.cs
@@ -12,7 +12,7 @@ namespace Aseprite2Unity.Editor
     [ScriptedImporter(4, new string[] { "aseprite", "ase" })]
     public class AsepriteImporter : ScriptedImporter, IAseVisitor
     {
-        public const string Version = "1.1.0";
+        public const string Version = "1.1.1";
 
         private readonly static Color m_TransparentColor = new Color32(0, 0, 0, 0);
 
